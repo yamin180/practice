b202bb30ada52fe3ece74e4c9b49a3a9e180224f
Yamin <mohammadyamin159263@gmail.com>
2025-09-10 12:33:18 +0600

    Con.With function

--- string.C
+++ string.C
@@ -2,15 +2,11 @@
 #include<string.h>
 int main()
 {
-	char a[38]="yamin ";
-	char b[38]="eushan";
-	int j;
-	int len_a=strlen(a);
-	int len_b=strlen(b);
-	int length=len_a+len_b;
-	for(int i=len_a,j=0;i<length;i++,j++){
-		a[i]=b[j];
-	}
+	char a[38]="With ";
+	char b[38]="Library Function";
+	strcat(a,b);
+	
 printf("%s",a);
-a[length]='\0';
-} 
+}
+
+
